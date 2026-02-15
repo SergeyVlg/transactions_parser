c1d064983b515715f90502c4f9f348e6fe5c7af4
Sergey Rusakov <s.rusakov@kefirgames.com>
2026-02-16 01:37:16 +0300

    add: macros to generate bit fields for transaction structure fields

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,6 @@ use std::error::Error;
 use std::io::{Read, Write};
 
 trait Parser<E: Error> : Sized {
-    fn from_read<R: Read>(reader: &mut R) -> Result<Self, E>;
+    fn from_read<R: Read>(reader: &mut R) -> Result<Vec<Self>, E>;
     fn write_to<W: Write>(&mut self, writer: &mut W) -> Result<(), E>;
 }
--- src/txt_format.rs
+++ src/txt_format.rs
@@ -1,13 +1,29 @@
 pub mod txt_format {
+    use crate::Parser;
     use std::error::Error;
     use std::fmt::{Display, Formatter};
     use std::io::{BufRead, BufReader, Read, Write};
-    use crate::Parser;
+    use std::str::FromStr;
+    use derive_macros::GenerateProcessedFields;
 
     enum TransactionType {
         Deposit,
         Transfer,
         Withdrawal
+    }
+
+    impl FromStr for TransactionType {
+        type Err = ();
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            match s {
+                "DEPOSIT" => Ok(TransactionType::Deposit),
+                "TRANSFER" => Ok(TransactionType::Transfer),
+                "WITHDRAWAL" => Ok(TransactionType::Withdrawal),
+
+                _ => Err(()),
+            }
+        }
     }
 
     enum TransactionStatus {
@@ -16,6 +32,21 @@ pub mod txt_format {
         Failure
     }
 
+    impl FromStr for TransactionStatus {
+        type Err = ();
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            match s {
+                "PENDING" => Ok(TransactionStatus::Pending),
+                "FAILURE" => Ok(TransactionStatus::Failure),
+                "SUCCESS" => Ok(TransactionStatus::Success),
+
+                _ => Err(()),
+            }
+        }
+    }
+
+    #[derive(GenerateProcessedFields)]
     struct YPBankTextRecord {
         id: u32,
         transaction_type: TransactionType,
@@ -29,10 +60,15 @@ pub mod txt_format {
 
     #[derive(Debug)]
     enum TextRecordError {
-        WrongLineFormat { line_index: u32 },
-        MissingColonAfterKey { line_index: u32 },
-        UnexpectedKey { line_index: u32 },
+        WrongLineFormat { line_index: usize },
+        MissingColonAfterKey { line_index: usize },
+        UnexpectedKey { line_index: usize },
         ReadLineError(std::io::Error),
+        ParseError { wrong_field: String, line_index: usize },
+        NotAllFieldsSet { line_index: usize },
+        ExcessFields,
+        WrongFieldsCount { line_index: usize },
+        SourceIsEmpty
     }
 
     impl Display for TextRecordError {
@@ -44,44 +80,129 @@ pub mod txt_format {
     impl Error for TextRecordError {}
 
     impl Parser<TextRecordError> for YPBankTextRecord {
-        fn from_read<R: Read>(reader: &mut R) -> Result<Self, TextRecordError> {
-            let mut buff_reader = BufReader::new(reader);
-            let mut line_index: u32 = 0;
+        fn from_read<R: Read>(reader: &mut R) -> Result<Vec<YPBankTextRecord>, TextRecordError> {
+            let mut records = vec![];
+            let buff_reader = BufReader::new(reader);
+            let mut current_transaction_raw: Vec<String> = vec![];
 
-            for line in buff_reader.lines() {
-                if let Ok(line) = line {
-                    if line.trim().starts_with('#') {
-                        continue;
-                    }
+            for (index, line_result) in buff_reader.lines().enumerate() {
+                let line = line_result.map_err(TextRecordError::ReadLineError)?;
+                let trimmed_line = line.trim();
 
-                    let line_values: Vec<&str> = line.trim().split(' ').collect();
-
-                    if line_values.len() <= 1 {
-                        return Err(TextRecordError::WrongLineFormat { line_index })
-                    }
-
-                    let key = line_values[0];
-                    let Some(':') = key.chars().nth(key.chars().count() - 1) else {
-                        return Err(TextRecordError::MissingColonAfterKey { line_index })
-                    };
-                    let key = key.trim_matches(':');
-
-                    match key {
-                        "TX_ID" => line_index = line_index + 1,
-                        _ => return Err(TextRecordError::UnexpectedKey { line_index })
-                    }
-
-                } else {
-                    return Err(TextRecordError::ReadLineError(line.unwrap_err()));
+                if trimmed_line.starts_with('#') {
+                    continue;
                 }
 
-                line_index += 1;
+                if trimmed_line.is_empty() {
+                    if !current_transaction_raw.is_empty() {
+                        let record = YPBankTextRecord::read_transaction(&current_transaction_raw, index)?;
+                        records.push(record);
+                        current_transaction_raw.clear();
+                    }
+                } else {
+                    current_transaction_raw.push(trimmed_line.to_string());
+                }
             }
-            Ok(());
+
+            if !current_transaction_raw.is_empty() {
+                let record = YPBankTextRecord::read_transaction(&current_transaction_raw, 0)?;
+                records.push(record);
+            }
+
+            if records.is_empty() {
+                Err(TextRecordError::SourceIsEmpty)
+            } else {
+                Ok(records)
+            }
         }
 
         fn write_to<W: Write>(&mut self, writer: &mut W) -> Result<(), &(dyn Error + 'static)> {
             todo!()
         }
+    }
+
+    impl YPBankTextRecord {
+        const FIELDS_COUNT: usize = 8;
+
+        fn default() -> YPBankTextRecord {
+            YPBankTextRecord {
+                id: 0,
+                transaction_type: TransactionType::Deposit,
+                from_user_id: 0,
+                to_user_id: 0,
+                amount: 0,
+                timestamp: 0,
+                transaction_status: TransactionStatus::Pending,
+                description: "".to_string()
+            }
+        }
+
+        fn read_transaction(raw_data: &[String], start_line_index: usize) -> Result<Self, TextRecordError> {
+            if raw_data.len() != Self::FIELDS_COUNT {
+                return Err(TextRecordError::WrongFieldsCount { line_index: start_line_index });
+            }
+
+            let mut processed_fields: ProcessedFields = ProcessedFields::empty();
+            let mut record = YPBankTextRecord::default();
+            let line_index = start_line_index;
+
+            for (offset, raw_string) in raw_data.iter().enumerate() {
+                let line_index = start_line_index + offset;
+
+                let (key, value) = raw_string
+                    .split_once(':')
+                    .ok_or(TextRecordError::WrongLineFormat { line_index })?;
+
+                let key = key.trim();
+                let value = value.trim();
+
+                match key {
+                    "TX_ID" => {
+                        record.id = parse(key, value, line_index)?;
+                        processed_fields |= ProcessedFields::Id;
+                    }
+                    "TX_TYPE" => {
+                        record.transaction_type = parse(key, value, line_index)?;
+                        processed_fields |= ProcessedFields::TransactionType;
+                    }
+                    "FROM_USER_ID" => {
+                        record.from_user_id = parse(key, value, line_index)?;
+                        processed_fields |= ProcessedFields::FromUserId;
+                    }
+                    "TO_USER_ID" => {
+                        record.to_user_id = parse(key, value, line_index)?;
+                        processed_fields |= ProcessedFields::ToUserId;
+                    }
+                    "AMOUNT" => {
+                        record.amount = parse(key, value, line_index)?;
+                        processed_fields |= ProcessedFields::Amount;
+                    }
+                    "TIMESTAMP" => {
+                        record.timestamp = parse(key, value, line_index)?;
+                        processed_fields |= ProcessedFields::Timestamp;
+                    }
+                    "STATUS" => {
+                        record.transaction_status = parse(key, value, line_index)?;
+                        processed_fields |= ProcessedFields::TransactionStatus;
+                    }
+                    "DESCRIPTION" => {
+                        record.description = parse(key, value, line_index)?;
+                        processed_fields |= ProcessedFields::Description;
+                    }
+                    _ => return Err(TextRecordError::UnexpectedKey { line_index }),
+                }
+            }
+
+            if processed_fields.is_all() {
+                Ok(record)
+            }
+            else {
+                Err(TextRecordError::NotAllFieldsSet { line_index })
+            }
+        }
+    }
+
+    fn parse<T: FromStr>(key: &str, value: &str, line_index: usize) -> Result<T, TextRecordError> {
+        value.parse().or_else(|_| return Err(TextRecordError::ParseError { wrong_field: key.to_string(), line_index }))
     }
 }
